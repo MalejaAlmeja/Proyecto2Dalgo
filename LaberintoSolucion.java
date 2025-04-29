5245e499d3039e4cab284e3e289f32cfa84d4dd1
Daniela González <147632134+99DGO@users.noreply.github.com>
2025-04-28 21:58:12 -0500

    first commit y de paso creacion de listas de aristas

--- LaberintoSolucion.java
+++ LaberintoSolucion.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class LaberintoSolucion
@@ -7,7 +8,69 @@ public class LaberintoSolucion
         ArrayList<String> camino = new ArrayList<String>();
         //En plataforma, los valores posibles son: null (no hay nada), "R" (robot), "k" (plataformas que se pueden saltar) o "FIN" (llegada al villano final)
 
+        ArrayList<int[]> aristas =crearAristas(plataformas, energia, plataforma);
+        
         return camino;
+    }
+
+    /*
+     * Crea una lista de aristas del siguiente formato: [origen, destino, costo normal, costo energia]
+     */
+    public static  ArrayList<int[]>  crearAristas(int plataformas, int energia, String[] plataforma)
+    {
+        ArrayList<int[]> aristas = new ArrayList<int[]>();
+
+
+        for (int i = 1; i < plataformas; i++) 
+        {
+            //Añado aristas de caminar adelante y atras
+            if (!plataforma[i].equals("R") && !plataforma[i+1].equals("R") ) 
+            {
+                int[] edge1= {i, i+1, 1, 0};
+                int[] edge2= {i+1, i, 1, 0};
+                aristas.add(edge1);
+                aristas.add(edge2);
+            }
+            
+            //Añado aristas de saltar
+            if (plataforma[i] != null && !plataforma[i].equals("R") )
+            {
+                int numSaltos= Integer.parseInt(plataforma[i]);
+                if (i-numSaltos >=1)
+                {
+                    if (!plataforma[i-numSaltos].equals("R"))
+                    {
+                        int[] edge={i, i-numSaltos, 1, 0};
+                        aristas.add(edge);  
+                    }
+                }
+                if (i+numSaltos<=plataformas)
+                {
+                    if (!plataforma[i+numSaltos].equals("R"))
+                    {
+                        int[] edge={i, i+numSaltos, 1, 0};
+                        aristas.add(edge);  
+                    }
+                }
+            }
+
+            //Añado aristas de teletransportación
+            if (!plataforma[i].equals("R") && energia>1)
+            {
+                for (int i2 = 2; i2 <= energia; i2++)
+                {
+                    if (!plataforma[i2+i].equals("R"))
+                    {
+                        int[] edge={i, i2+i, 1, i2};
+                        aristas.add(edge);  
+                    }
+                }
+            }
+            
+
+        }
+
+        return aristas;
     }
 
     public static void main(String[] args) {
